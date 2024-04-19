9bfefc03b2c37deda8f946123008e84b72c89b1a
polina <polina@DESKTOP-TTJ3VKJ>
2024-04-19 15:01:57 +0300

    Добавьте файлы проекта.

---
+++ Povozka/Components/Data/ExistingAccounts.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Povozka.Components.Data
+{
+    public static class ExistingAccounts
+    {
+        public static List<User> Accounts { get; set; }
+        private static string path = "Components//Data//Accounts.txt";
+        public static User CurrentUser { get; set; }
+
+        public static void GetAccounts()
+        {
+            Accounts = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(path));
+        }
+
+        public static void AddAccount(User account)
+        {
+            Accounts.Add(account);
+            File.WriteAllText(path, JsonConvert.SerializeObject(Accounts));
+        }
+    }
+}
---
+++ Povozka/Components/Data/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Povozka.Components.Data
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Guid ID { get; set; }
+
+        public User(string username, string login, string pass)
+        {
+            ID = Guid.NewGuid();
+            Username = username;
+            Login = login;
+            Password = pass;
+        }
+    }
+}
